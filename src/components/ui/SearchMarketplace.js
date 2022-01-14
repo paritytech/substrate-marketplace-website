07940e9be46844fbc5653a06372894bf0bdc7de7
Imad Arain <arain.imad@gmail.com>
2022-01-14 11:41:09 +0100

    Search UI hover + padding

--- src/components/ui/SearchMarketplace.js
+++ src/components/ui/SearchMarketplace.js
@@ -7,14 +7,15 @@ export default function SearchMarketplace() {
   return (
     <div
       className={cx(
-        `flex items-center justify-between mx-auto p-2 border-b-2 border-substrateGray cursor-text max-w-2xl`,
+        `flex items-center justify-between mx-auto p-2 border-2 border-transparent border-b-substrateGray cursor-text max-w-2xl`,
+        `lg:p-6 lg:rounded`,
         `duration-150 ease-in-out hover:border-substrateDark`
       )}
     >
-      <p className="mb-0 pr-4 text-sm text-substrateDark dark:text-white text-opacity-25 dark:text-opacity-90">
-        Search Documentation
+      <p className="mb-0 pr-4 text-sm text-substrateDark dark:text-white text-opacity-25 lg:text-opacity-60 dark:text-opacity-90">
+        Search Marketplace
       </p>
-      <Icon name="searchIcon" className="h-4 w-4 fill-current text-substrateDark dark:text-white" />
+      <Icon name="searchIcon" className="h-4 w-4 lg:h-6 lg:w-6 fill-current text-substrateDark dark:text-white" />
     </div>
   );
 }
