affca59605d99f8e3de63f69ec62a1d11abd6bb1
Imad Arain <arain.imad@gmail.com>
2022-01-14 15:29:37 +0100

    removing capitalization from homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home({ data }) {
       <Section className="sm:text-center">
         <h1 className="mb-8 text-3xl sm:text-5xl md:text-6xl font-title font-extrabold">Substrate Marketplace</h1>
         <div className="sm:max-w-lg mx-auto mb-10">
-          <p className="font-medium capitalize leading-8">
+          <p className="font-medium leading-8">
             Where blockchain innovators discover & share reusable pallets for use with Parity Substrate, the open-source
             Blockchain framework.
           </p>
