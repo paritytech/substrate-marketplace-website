59a4001f2a41968db46e7a0c85e237eabbb0fbfa
Imad Arain <arain.imad@gmail.com>
2022-01-14 11:51:34 +0100

    dark mode updates to search ui

--- src/components/ui/SearchMarketplace.js
+++ src/components/ui/SearchMarketplace.js
@@ -9,10 +9,15 @@ export default function SearchMarketplace() {
       className={cx(
         `flex items-center justify-between mx-auto p-2 border-2 border-transparent border-b-substrateGray cursor-text max-w-2xl`,
         `lg:p-6 lg:rounded`,
-        `duration-150 ease-in-out hover:border-substrateDark`
+        `duration-150 ease-in-out hover:border-substrateDark dark:hover:border-substrateGray`
       )}
     >
-      <p className="mb-0 pr-4 text-sm text-substrateDark dark:text-white text-opacity-25 lg:text-opacity-60 dark:text-opacity-90">
+      <p
+        className={cx(
+          `mb-0 pr-4 text-sm text-substrateDark dark:text-white text-opacity-25 dark:text-opacity-90`,
+          `lg:text-opacity-60 lg:text-xl`
+        )}
+      >
         Search Marketplace
       </p>
       <Icon name="searchIcon" className="h-4 w-4 lg:h-6 lg:w-6 fill-current text-substrateDark dark:text-white" />
