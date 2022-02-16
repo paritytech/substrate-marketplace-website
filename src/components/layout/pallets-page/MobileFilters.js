d0bd697f40134234f5d0ed20dd2e2e5c8fbfdeba
Imad Arain <arain.imad@gmail.com>
2022-02-16 17:48:55 +0100

    mobilefilter dark mode update

--- src/components/layout/pallets-page/MobileFilters.js
+++ src/components/layout/pallets-page/MobileFilters.js
@@ -19,7 +19,11 @@ export default function MobileFilters({
     <>
       <div className="flex flex-col mb-6">
         <label className="text-lg mb-3">Filter by Type</label>
-        <select value={selectedCategory} onChange={event => setSelectedCategory(event.target.value)}>
+        <select
+          className="dark:bg-substrateDark"
+          value={selectedCategory}
+          onChange={event => setSelectedCategory(event.target.value)}
+        >
           <option value="all">All</option>
           {sortCategories(categories).map((cat, index) => (
             <option key={index} value={cat.name} className="capitalize">
@@ -30,7 +34,11 @@ export default function MobileFilters({
       </div>
       <div className="flex flex-col mb-6">
         <label className="text-lg mb-3">Filter by Substrate Version</label>
-        <select value={selectedVersion} onChange={event => setSelectedVersion(event.target.value)}>
+        <select
+          className="dark:bg-substrateDark"
+          value={selectedVersion}
+          onChange={event => setSelectedVersion(event.target.value)}
+        >
           {versions.map((version, index) => {
             return (
               <option key={index} value={version === 'Substrate 3.0' ? 'VERSION_3_0' : 'VERSION_2_0'}>
