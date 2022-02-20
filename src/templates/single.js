070279613f1b88f80e21c33c6c74d731a644a3df
Imad Arain <arain.imad@gmail.com>
2022-02-20 17:09:40 +0100

    aligned layout + readme content from backend

--- src/templates/single.js
+++ src/templates/single.js
@@ -1,7 +1,7 @@
-import cx from 'classnames';
 import { graphql } from 'gatsby';
 import React from 'react';
 
+import Section from '../components/layout/Section';
 import Sidebar from '../components/layout/single-template/Sidebar';
 import Layout from '../components/site/Layout';
 import NavBreadcrumb from '../components/site/NavBreadcrumb';
@@ -10,31 +10,37 @@ import ProjectLogo from '../components/ui/ProjectLogo';
 
 export default function SingularPage({ pageContext }) {
   const { node, section } = pageContext;
-  const { name, description } = node;
+  const { name, description, readmeContent } = node;
 
   return (
     <Layout>
       <SEO title={name} />
-      <article className={cx(`container flex flex-col lg:flex-row xl:px-12`)}>
-        <div className="flex-grow w-full mt-2 mb-10 lg:mb-20 pr-4 underline-animate underline-animate-thin">
-          <div className="mb-12">
-            <NavBreadcrumb />
-          </div>
-          <div className="mb-12">
-            <div className="flex flex-col lg:flex-row lg:items-center">
-              {section === 'projects' && (
-                <ProjectLogo
-                  projectName={name}
-                  className="w-12 h-12 lg:w-[60px] lg:h-[60px] mr-5 p-0.5 object-contain rounded-full dark:bg-gray-300"
-                />
-              )}
-              <h1 className="font-bold mb-0 lg:text-5xl">{name}</h1>
+      <Section>
+        <article className="lg:flex">
+          <div className="lg:flex-grow mb-10 lg:mb-20 pr-4 underline-animate underline-animate-thin">
+            <div className="mb-12">
+              <NavBreadcrumb />
             </div>
+            <div className="mb-12">
+              <div className="flex flex-col lg:flex-row lg:items-center">
+                {section === 'projects' && (
+                  <ProjectLogo
+                    projectName={name}
+                    className="w-12 h-12 lg:w-[60px] lg:h-[60px] mr-5 p-0.5 object-contain rounded-full dark:bg-gray-300"
+                  />
+                )}
+                <h1 className="font-bold mb-0 lg:text-5xl">{name}</h1>
+              </div>
+            </div>
+            {readmeContent ? (
+              <div className="max-w-4xl markdown-body" dangerouslySetInnerHTML={{ __html: readmeContent }}></div>
+            ) : (
+              <p className="max-w-4xl markdown-body">{description}</p>
+            )}
           </div>
-          <p className="max-w-4xl">{description}</p>
-        </div>
-        <Sidebar data={node} section={section} />
-      </article>
+          <Sidebar data={node} section={section} />
+        </article>
+      </Section>
     </Layout>
   );
 }
