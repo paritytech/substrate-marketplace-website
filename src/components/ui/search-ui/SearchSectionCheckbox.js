aeda50830bb846c01f59da929e58389a53d1d741
Imad Arain <arain.imad@gmail.com>
2022-02-28 12:38:42 +0100

    search checkbox refactor

--- src/components/ui/search-ui/SearchSectionCheckbox.js
+++ src/components/ui/search-ui/SearchSectionCheckbox.js
@@ -5,14 +5,17 @@ export default function SearchSectionCheckbox({ isChecked, setIsChecked, childre
     setIsChecked(event.target.checked);
   };
   return (
-    <label className="inline-flex items-center cursor-pointer mr-16">
+    <div className="inline-flex items-center mr-16">
       <input
-        className="h-4 w-4 md:h-[18px] md:w-[18px] rounded bg-white text-substrateDark border-2 focus:ring-0 "
+        className="cursor-pointer h-4 w-4 md:h-[18px] md:w-[18px] rounded bg-white text-substrateDark border-2 focus:ring-0 "
         type="checkbox"
+        id={children}
         checked={isChecked}
         onChange={e => handleChange(e)}
       />
-      <span className="text-base md:text-lg font-semibold pl-2">{children}</span>
-    </label>
+      <label className="cursor-pointer text-base md:text-lg font-semibold pl-2" htmlFor={children}>
+        {children}
+      </label>
+    </div>
   );
 }
