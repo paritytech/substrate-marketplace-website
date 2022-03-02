925c13a3d6e9301ec0abf8bce48724fd8eff127b
Imad Arain <arain.imad@gmail.com>
2022-03-02 09:56:39 +0100

    added trailingslash to breadcrumb + disabling current page

--- src/components/site/NavBreadcrumb.js
+++ src/components/site/NavBreadcrumb.js
@@ -6,19 +6,24 @@ import DataContext from '../../contexts/DataContext';
 export default function NavBreadcrumb() {
   return (
     <DataContext.Consumer>
-      {({ pageContext, pathArray, pageTitle }) => (
-        <Breadcrumb
-          crumbs={pageContext.breadcrumb.crumbs}
-          crumbSeparator=""
-          crumbLabel={pageTitle}
-          /* Enable hiddenCrumbs to hide specific links */
-          // hiddenCrumbs={['/']}
-          /* keep parent as a link and disable children links (eg):
+      {({ pageContext, pageTitle }) => {
+        const {
+          breadcrumb: { location },
+        } = pageContext;
+        return (
+          <Breadcrumb
+            crumbs={pageContext.breadcrumb.crumbs}
+            crumbSeparator=""
+            crumbLabel={pageTitle}
+            /* Enable hiddenCrumbs to hide specific links */
+            // hiddenCrumbs={['/']}
+            /* keep parent as a link and disable children links (eg):
             "/<ecosystem>/resources/awesome-substrate" and "/<ecosystem>/resources/" */
-          disableLinks={[`/${pathArray[0]}/${pathArray[1]}/${pathArray[2]}`, `/${pathArray[0]}/${pathArray[1]}`]}
-          className="breadcrumb__list breadcrumb__list__item breadcrumb__separator breadcrumb__link breadcrumb__link__active"
-        />
-      )}
+            disableLinks={[`${location}`]}
+            className="breadcrumb__list breadcrumb__list__item breadcrumb__separator breadcrumb__link breadcrumb__link__active"
+          />
+        );
+      }}
     </DataContext.Consumer>
   );
 }
