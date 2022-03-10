7ab2a1e323cfc79e13ae1e54bb9916d7c6bdfda2
Imad Arain <arain.imad@gmail.com>
2022-03-10 13:18:32 +0100

    adding delay to noResults window

--- src/components/layout/sub-pages/CardsContainer.js
+++ src/components/layout/sub-pages/CardsContainer.js
@@ -38,7 +38,9 @@ export default function CardsContainer({ data, section, selectedVersion, searchQ
 
   useEffect(() => {
     displayedData.length > 0 ? setDataAvailable(true) : setDataAvailable(false);
-    displayedData.length === 0 ? setNoResults(true) : setNoResults(false);
+    setTimeout(() => {
+      displayedData.length === 0 ? setNoResults(true) : setNoResults(false);
+    }, 100);
   }, [displayedData]);
 
   return (
