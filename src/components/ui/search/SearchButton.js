d15c8fc7b42df0bcc56f164c9fc8e84be15444bc
rmnprkrl <rmnprkrl@gmail.com>
2022-06-27 20:16:10 +0200

    cleanup button css

--- src/components/ui/search/SearchButton.js
+++ src/components/ui/search/SearchButton.js
@@ -5,13 +5,11 @@ import Icon from '../../default/Icon';
 export default function SearchButton({ handleSearchOpen }) {
   return (
     <button
-      className="primary-button group bg-substrateGreen inline-flex relative rounded-md px-8 py-4 text-xl hover:opacity-80"
+      className="bg-substrateGreen inline-flex items-center relative rounded-md px-8 py-4 text-xl hover:opacity-80"
       onClick={handleSearchOpen}
     >
       <span className="font-bold text-white mb-0 mr-4">Search Marketplace</span>
-      <div className="flex h-full">
-        <Icon name="searchIcon" className="h-4 w-4 lg:h-6 lg:w-6 fill-current text-white dark:text-white" />
-      </div>
+      <Icon name="searchIcon" className="h-4 w-4 lg:h-6 lg:w-6 fill-current text-white dark:text-white" />
     </button>
   );
 }
